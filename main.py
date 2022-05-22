2e361bb49e05b9add1fa22127d13598ec3b08a2e
Maxik132 <93473242+Maxik132@users.noreply.github.com>
2022-05-22 14:28:35 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import logging
+from aiogram import executor
+from create_bot import dp
+from Handlers import register_handlers
+
+
+logging.basicConfig(level=logging.INFO)
+
+register_handlers(dp)
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
